62b1833b4b218afd757afb2b359f0d6b6c0d1f4d
Andrew <sseluyindaeva@gmail.com>
2025-06-29 22:23:57 +0000

    Corrected the landing page

--- detector/admin.py
+++ detector/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import ChickenAnalysis
 # Register your models here.
+
+@admin.register(ChickenAnalysis)
+class ChickenAnalysisAdmin(admin.ModelAdmin):
+    list_display = ['id', 'predicted_disease', 'confidence_score', 'analysis_date']
+    list_filter = ['predicted_disease', 'analysis_date']
+    readonly_fields = ['id', 'analysis_date']
+    search_fields = ['predicted_disease']
