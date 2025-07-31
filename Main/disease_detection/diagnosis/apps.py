9cad4d8f5653755c2cc81adbcf911525f8a96da1
Andrew <sseluyindaeva@gmail.com>
2025-07-31 06:54:14 +0300

    morning

--- Main/disease_detection/diagnosis/apps.py
+++ Main/disease_detection/diagnosis/apps.py
@@ -1,9 +1,14 @@
 from django.apps import AppConfig
+import sys
 
 class DiagnosisConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'diagnosis'
 
     def ready(self):
+        # Avoid loading the model during management commands
+        if 'manage.py' in sys.argv:
+            return
+
         from . import views  # Import views here to avoid circular dependency
         views.load_model()
